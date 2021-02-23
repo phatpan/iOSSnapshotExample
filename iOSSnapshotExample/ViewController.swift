89b119a67df44fc47c406cb3b6383a9e9be4c2a3
Phatcharaphan Ananpreechakun <phat.pan@gmail.com>
2021-02-23 23:01:06 +0700

    Initial Commit

---
+++ iOSSnapshotExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSSnapshotExample
+//
+//  Created by Phatcharaphan Ananpreechakun on 23/2/2564 BE.
+//  Copyright © 2564 Phatcharaphan Ananpreechakun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ iOSSnapshotExampleTests/iOSSnapshotExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOSSnapshotExampleTests.swift
+//  iOSSnapshotExampleTests
+//
+//  Created by Phatcharaphan Ananpreechakun on 23/2/2564 BE.
+//  Copyright © 2564 Phatcharaphan Ananpreechakun. All rights reserved.
+//
+
+import XCTest
+@testable import iOSSnapshotExample
+
+class iOSSnapshotExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
