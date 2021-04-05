3fd164593cffdbdb2afe8d2b3f76c3e8274c7dec
Phatcharaphan Ananpreechakun <phat.pan@gmail.com>
2021-04-05 11:16:54 +0700

    untrack Pods folder

--- Pods/Target Support Files/Pods-iOSSnapshotExample-iOSSnapshotExampleUITests/Pods-iOSSnapshotExample-iOSSnapshotExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSSnapshotExample_iOSSnapshotExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_iOSSnapshotExample_iOSSnapshotExampleUITests
-@end
--- Pods/Target Support Files/Pods-iOSSnapshotExampleTests/Pods-iOSSnapshotExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSSnapshotExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_iOSSnapshotExampleTests
-@end
